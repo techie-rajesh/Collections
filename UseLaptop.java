342f5b4e7327254b86cf88a37a0d203c48a69325
techie-rajesh <vrajeshraju1@gmail.com>
2023-01-04 16:58:40 +0530

    add program

---
+++ UseLaptop.java
@@ -0,0 +1,113 @@
+package Practice;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+import java.util.stream.Collectors;
+ class Laptop 
+{
+	private String brand;
+	private int price;
+	private boolean isWarranty;
+	private boolean isTouchScreen;
+	private String color;
+	private int memory;
+	private float stoarge;
+	private int serialNo;
+	public Laptop(String brand, int price, boolean isWarranty, boolean isTouchScreen, String color, int memory,
+			float stoarge,int serialNo) {
+		super();
+		this.brand = brand;
+		this.price = price;
+		this.isWarranty = isWarranty;
+		this.isTouchScreen = isTouchScreen;
+		this.color = color;
+		this.memory = memory;
+		this.stoarge = stoarge;
+		this.serialNo=serialNo;
+	}
+	public String getBrand() {
+		return brand;
+	}
+	public void setBrand(String brand) {
+		this.brand = brand;
+	}
+	public int getPrice() {
+		return price;
+	}
+	public void setPrice(int price) {
+		this.price = price;
+	}
+	public boolean isWarranty() {
+		return isWarranty;
+	}
+	public void setWarranty(boolean isWarranty) {
+		this.isWarranty = isWarranty;
+	}
+	public boolean isTouchScreen() {
+		return isTouchScreen;
+	}
+	public void setTouchScreen(boolean isTouchScreen) {
+		this.isTouchScreen = isTouchScreen;
+	}
+	public String getColor() {
+		return color;
+	}
+	public void setColor(String color) {
+		this.color = color;
+	}
+	public int getMemory() {
+		return memory;
+	}
+	public void setMemory(int processor) {
+		this.memory = processor;
+	}
+	public float getStoarge() {
+		return stoarge;
+	}
+	public void setStoarge(float stoarge) {
+		this.stoarge = stoarge;
+	}
+	public void setSerialNo(int serialNo)
+	{
+		this.serialNo=serialNo;
+	}
+	public int getSerialNo()
+	{
+		return serialNo;
+	}
+	@Override
+	public String toString() {
+		return "Laptop [brand=" + brand + ", price=" + price + ", isWarranty=" + isWarranty + ", isTouchScreen="
+				+ isTouchScreen + ", color=" + color + ", Memory=" + memory + ", stoarge=" + stoarge + "]";
+	}
+}
+public class UseLaptop {
+	public static void main(String[] args) {
+		ArrayList<Laptop> laptops=new ArrayList<>();
+		laptops.add(new Laptop("Asus", 54000, true, false, "Black", 6, 1.0f,1));
+		laptops.add(new Laptop("Hp", 64000, true, true, "Cilver", 8, 2.0f,2));
+		laptops.add(new Laptop("Dell", 44000, false, false, "White", 6, 0.5f,3));
+		laptops.add(new Laptop("Lenova", 54000, true, false, "Grey", 6, 1.0f,4));
+		laptops.add(new Laptop("Acer", 30000, false, false, "White", 4, 2.0f,5));
+		laptops.add(new Laptop("MSI", 74000, true, true, "Red", 12, 3.0f,6));
+		laptops.add(new Laptop("Ipad", 54000, true, false, "Black", 8, 1.0f,7));
+		laptops.add(new Laptop("MacBook", 98000, true,true, "Blue", 4, 3.0f,8));
+		laptops.add(new Laptop("Asus", 54000, true, false, "Black", 64, 2.0f,9));
+		laptops.add(new Laptop("Mi", 74000, true, true, "Blue", 12, 3.5f,10));
+		
+		//Map<Integer, Laptop> cvt1=laptops.stream().collect(Collectors.toMap(x->x.getSerialNo(), y->y));
+		//cvt1.forEach((x,y)->System.out.println(x+" "+y));
+		//Set<Laptop> cvt2=laptops.stream().collect(Collectors.toSet());
+		//cvt2.forEach(x->System.out.println(x));
+		//List<Laptop> h_Sal=laptops.stream().filter(x->x.getPrice()>50000).distinct().collect(Collectors.toList());
+		//h_Sal.forEach(x->System.out.println(x));
+		Laptop max_Price=laptops.stream().max((x,y)->x.getPrice()>y.getPrice()?1:-1).get();
+		System.out.println("Maximum price: "+max_Price.getPrice());
+		Laptop min_Price=laptops.stream().min((x,y)->x.getPrice()>y.getPrice()?1:-1).get();
+		System.out.println("Minimum Price: "+min_Price.getPrice());
+		
+	}
+}
+
